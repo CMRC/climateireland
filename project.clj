8b63fc6f73a79afd194dad10af9dbc6adf09191b
Anthony Patterson <a.patterson@ucc.ie>
2011-10-24 13:07:46 +0100

    using netcdf-clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject hello-noir "0.1.0-SNAPSHOT"
+(defproject climateireland "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [enlive "1.0.0"]
-                           [noir "1.2.0"]]
+                           [noir "1.2.0"]
+                           [netcdf-clj "0.0.3-SNAPSHOT"]]
+            :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
             :main ci.server)
--- src/ci/views/welcome.clj
+++ src/ci/views/welcome.clj
@@ -3,13 +3,20 @@
   (:use noir.core
         hiccup.core
         hiccup.page-helpers
-	net.cgrand.enlive-html))
+	net.cgrand.enlive-html
+        netcdf.geo-grid
+        clojure.pprint))
 
 (deftemplate welcome "ci/views/welcome.html"  
-  [blurb]
+  [blurb map]
 
   [:#blurb]
-  (content blurb))
+  (content blurb)
+  
+  [:#map]
+  (content map))
 
 (defpage "/welcome" []
-         (welcome "Welcome to hello-noir"))
+  (welcome "Welcome to hello-noir"
+           (with-open-geo-grid [grid "http://badc.nerc.ac.uk/help/formats/netcdf/simple.nc" "temp"]
+             (apply str (interpose \, (read-seq grid))))))
