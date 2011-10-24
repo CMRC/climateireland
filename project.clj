0a0e433263716fdb8d5b8d2cef7cb6540bd0b3a7
Anthony Patterson <a.patterson@ucc.ie>
2011-10-24 09:23:30 +0100

    merged project file

--- project.clj
+++ project.clj
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-(defproject enlive-tutorial "0.1.0-SNAPSHOT"
-  :description "Enlive Tutorial"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [ring "0.2.5"]
-                 [hiccup "0.2.3"]
-                 [net.cgrand/moustache "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :source-path "src"
-  :main tutorial.template1)
-=======
 (defproject hello-noir "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [enlive "1.0.0"]
                            [noir "1.2.0"]]
             :main ci.server)
->>>>>>> 8b42bef7fbe6b7839c0a4f6179d4a1b23285e8dc
