ed7facf47d41d0b75444fada8101f4a5d5b3d6e0
Anthony Patterson <a.patterson@ucc.ie>
2011-10-26 17:25:29 +0100

    playing with averages

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
 (defproject climateireland "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [enlive "1.0.0"]
-                           [noir "1.2.0"]
-                           [netcdf-clj "0.0.3-SNAPSHOT"]]
-            :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
-            :main ci.server)
+  :description "FIXME: write this!"
+  :repositories {"osgeo-geotools" "http://download.osgeo.org/webdav/geotools"}
+  
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [enlive "1.0.0"]
+                 [noir "1.2.0"]
+                 [netcdf-clj "0.0.3-SNAPSHOT"]
+                 [org.geotools/gt-main "2.6.4"]
+                 [org.geotools/gt-shapefile "2.6.4"]
+                 [org.geotools/gt-epsg-hsql "2.6.4"]
+                 [org.geotools/gt-swing "2.6.4"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :main ci.server)
--- src/ci/views/netcdfmap.clj
+++ src/ci/views/netcdfmap.clj
@@ -2,11 +2,21 @@
   (:use netcdf.geo-grid))
   
 (def netcdfmap
-  (with-open-geo-grid [grid "http://badc.nerc.ac.uk/help/formats/netcdf/simple.nc" "temp"]
+  (with-open-geo-grid [grid "/home/anthony/CLM4-Barry/CLM4_Data/CLM4_A1B_1/CLM4_A1B_1_mean.nc" "PS"]
     (apply str "http://maps.googleapis.com/maps/api/staticmap?sensor=true&size=400x400"
            (map #(str "&markers=color:blue|label:" (% :value)
                       "|" (.getLatitude (% :location))
                       "," (.getLongitude (% :location)))
                 (read-seq grid)))))
   
+(with-open-geo-grid [grid "/home/anthony/CLM4-Barry/CLM4_Data/CLM4_A1B_1/CLM4_A1B_1_mean.nc" "PS"]
+  (doall (map (fn [long]
+                (/ (reduce #(+ %1 %2) 0
+                           (map #(% :value) (filter #(> (.getLatitude (% :location)) long)
+                                                    (filter-records (read-seq grid)))))
+                   (count (filter #(> (.getLatitude (% :location)) long)
+                                  (filter-records (read-seq grid)))))) (range 50 57))))  
+
+
+
 
