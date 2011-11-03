2ffe953175acc8674e328967d5744a1c927b6b55
Anthony Patterson <a.patterson@ucc.ie>
2011-11-03 15:29:23 +0000

    changed noir version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [enlive "1.0.0"]
-                           [noir "1.2.0"]
+                           [noir "1.1.0-SNAPSHOT"]
                            [netcdf-clj "0.0.3-SNAPSHOT"]]
             :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
             :main ci.server)
